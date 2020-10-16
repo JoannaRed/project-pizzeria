9c7f147279850c7db34a53264b99adbdd6cfb909
Joanna Baranowska <j.baranowska@hotmail.com>
2020-10-16 14:35:08 +0200

    Add changes

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,6 +1,6 @@
-import {settings, select, templates, classNames} from './settings.js';
-import CartProduct from './components/CartProduct.js';
-import utils from './utils.js';
+import {settings, select, templates, classNames} from '../settings.js';
+import CartProduct from '../components/CartProduct.js';
+import utils from '../utils.js';
 
 class Cart {
   constructor (element){
--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -1,6 +1,6 @@
-import { select, templates, classNames} from './settings.js';
-import AmountWidget from './components/AmountWidget.js';
-import utils from './utils.js';
+import { select, templates, classNames} from '../settings.js';
+import AmountWidget from '../components/AmountWidget.js';
+import utils from '../utils.js';
 
 
 class Product{
