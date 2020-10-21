199563f42fbc35a87c7924fe18bc41a2ed9ede81
Joanna Baranowska <j.baranowska@hotmail.com>
2020-10-21 13:09:29 +0200

    Add pick & hour

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -2,6 +2,8 @@ import {templates, select} from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
+
 
 export class Booking {
   constructor(element){
@@ -27,6 +29,8 @@ export class Booking {
     //thisBooking.appendChild(thisBooking.generatedDOM);
 
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
+
   }
 
   initWidget() {
@@ -35,6 +39,8 @@ export class Booking {
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+
   }
 
 }
